edaf7823a9b4634925191233ac99812db9025dc1
Icefyred <dcastilho001@hotmail.com>
2019-04-17 17:16:34 +0100

    Send Request only

--- src/main/java/pt/compta/http/proxy/traineeship/RequestHandler.java
+++ src/main/java/pt/compta/http/proxy/traineeship/RequestHandler.java
@@ -3,15 +3,41 @@ package pt.compta.http.proxy.traineeship;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.Socket;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 
 public class RequestHandler {
 
 	public void request(Socket clientSocket) throws IOException {
-		BufferedReader proxyToClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		PrintWriter printer = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+		BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		String requestLine = input.readLine();
 
+		String methodFromRequest = parseRequestMethod(requestLine);
+		String uriFromRequest = parseRequestUri(requestLine);
+
+		if (!(uriFromRequest.substring(0, 6).equals("http://"))) {
+			uriFromRequest = "http://" + uriFromRequest;
+		} else if (!(uriFromRequest.substring(0, 7).equals("https://"))) {
+			uriFromRequest = "https://" + uriFromRequest;
+		}
+
+		if (methodFromRequest.equals("GET")) {
+			HttpClient client = HttpClientBuilder.create().build();
+			HttpGet request = new HttpGet(uriFromRequest);
+			client.execute(request);
+		}
+	}
+
+	public String parseRequestMethod(String stringToExtract) {
+		String[] arrString = stringToExtract.split(" ");
+		return arrString[0];
+	}
+
+	public String parseRequestUri(String stringToExtract) {
+		String[] arrString = stringToExtract.split(" ");
+		return arrString[1];
 	}
 }
