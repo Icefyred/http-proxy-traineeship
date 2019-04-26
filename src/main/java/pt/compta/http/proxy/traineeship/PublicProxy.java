80241a07826b06c4773e767408d109768b94a578
Icefyred <dcastilho001@hotmail.com>
2019-04-26 11:50:41 +0100

    Changed the procedure of the server and created a public proxy to redirect the connection to it.

--- src/main/java/pt/compta/http/proxy/traineeship/PublicProxy.java
+++ src/main/java/pt/compta/http/proxy/traineeship/PublicProxy.java
@@ -1,5 +1,50 @@
 package pt.compta.http.proxy.traineeship;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.ServerSocket;
+import java.net.URL;
+
 public class PublicProxy {
+	private int publicProxyPort;
+	private ServerSocket serverSocket;
+
+	public PublicProxy(int portNumber) {
+		this.publicProxyPort = portNumber;
+		try {
+			serverSocket = new ServerSocket(publicProxyPort);
+			System.out.println("Public Proxy started on port..." + publicProxyPort);
+		} catch (Exception ex) {
+			System.err.println(ex.getMessage());
+		}
+	}
+
+	public void forward(String urlToForward) {
+		System.out.println("Connected to " + urlToForward);
+		try {
+			URL obj = new URL(urlToForward);
+			HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+
+			BufferedReader input = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+			StringBuilder stringBuilder = new StringBuilder();
+
+			String line = null;
+			while ((line = input.readLine()) != null) {
+				stringBuilder.append(line + "\n");
+			}
+			System.out.println(stringBuilder);
+			input.close();
+
+		} catch (Exception ex) {
+			System.err.println(ex.getMessage());
+		}
+	}
+
 	// TODO: Choose from a list a random and open Public Proxy to use
+	// inicializar para um proxy (check)
+	// sendMessage(stream) (check)
 }
+
+// http://www.sohu.com/
--- src/main/java/pt/compta/http/proxy/traineeship/RequestHandler.java
+++ src/main/java/pt/compta/http/proxy/traineeship/RequestHandler.java
@@ -5,53 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-
 public class RequestHandler {
 
 	public void request(Socket clientSocket) throws IOException {
+		int publicPortNumber = 8081;
 		BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		String requestLine = input.readLine();
 
-		String methodFromRequest = parseRequestMethod(requestLine);
 		String uriFromRequest = parseRequestUri(requestLine);
 
-		uriFromRequest = prependHttpProtocolIfNeeded(uriFromRequest);
+		PublicProxy publicProxy = new PublicProxy(publicPortNumber);
+		publicProxy.forward(uriFromRequest);
 
-		if (methodFromRequest.equals("GET")) {
-			HttpClient client = HttpClientBuilder.create().build();
-			HttpGet request = new HttpGet(uriFromRequest);
-			client.execute(request);
-		}
-	}
-
-	private String prependHttpProtocolIfNeeded(String uriFromRequest) {
-		String auxForHttpsProtocol = uriFromRequest.substring(0, 5);
-		String auxForHttpProtocol = uriFromRequest.substring(0, 4);
-
-		if (!doesItContainHttpProtocol(uriFromRequest, auxForHttpsProtocol, auxForHttpProtocol)) {
-			uriFromRequest = "http://" + uriFromRequest;
-		}
-		return uriFromRequest;
-	}
-
-	private boolean doesItContainHttpProtocol(String uri, String auxForHttpsProtocol, String auxForHttpProtocol) {
-		if (!auxForHttpsProtocol.equals("https") && !auxForHttpProtocol.equals("http")) {
-			return false;
-		}
-		return true;
-	}
-
-	public String parseRequestMethod(String stringToExtract) {
-		String[] arrString = splitRequestLine(stringToExtract);
-		return arrString[0];
+		/*
+		 * //abrir socket para proxy public //socket para host e port //após seja bem
+		 * sucedido pedir um request
+		 * 
+		 */
 	}
 
 	private String[] splitRequestLine(String stringToExtract) {
-		String[] arrString = stringToExtract.split(" ");
-		return arrString;
+		return stringToExtract.split(" ");
 	}
 
 	public String parseRequestUri(String stringToExtract) {
